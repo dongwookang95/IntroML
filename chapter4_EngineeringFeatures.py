c1e62d2a8ff6ead822ff77a7561835e3530a318c
dongwookang <d.kang-1@student.tudelft.nl>
2020-11-04 16:50:42 +0100

    finished chapter 4

--- chapter4_EngineeringFeatures.py
+++ chapter4_EngineeringFeatures.py
@@ -168,10 +168,249 @@ plt.legend(loc="best")
 from sklearn.svm import SVR
 
 for gamma in [1,10]:
-    svr = SVR(gamma=gamma).fot(X,y)
+    svr = SVR(gamma=gamma).fit(X,y)
     plt.plot(line,svr.predict(line),label='SVR gamma={}'.format(gamma))
-plt.plt(X[:,0],y,'o',c='k')
+plt.plot(X[:,0],y,'o',c='k')
 plt.ylabel("Regression output")
 plt.xlabel("Input feature")
 plt.legend(loc="best")
 
+
+
+
+# %%
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+boston = load_boston()
+X_train, X_test, y_train, y_test = train_test_split(boston.data, boston.target, random_state=0)
+
+#rescale data
+scaler = MinMaxScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+# %%
+poly = PolynomialFeatures(degree=2).fit(X_train_scaled)
+X_train_poly = poly.transform(X_train_scaled)
+X_test_poly = poly.transform(X_test_scaled)
+print("X_train.shape:{}".format(X_train.shape))
+print("X_train_poly.shape:{}".format(X_train_poly.shape))
+# %%
+print("Polynomial feature names:\n{}".format(poly.get_feature_names()))
+
+
+# %%
+from sklearn.linear_model import Ridge
+ridge = Ridge().fit(X_train_scaled,y_train)
+print("Score without interactions:{:.3f}".format(ridge.score(X_test_scaled, y_test)))
+ridge = Ridge().fit(X_train_poly,y_train)
+print("Score with interactions : {:.3f}".format(ridge.score(X_test_poly,y_test)))
+
+
+# %%
+from sklearn.ensemble import RandomForestRegressor
+
+rf = RandomForestRegressor(n_estimators=100).fit(X_train_scaled,y_train)
+print("Score without interactions:{:.3f}".format(rf.score(X_test_scaled,y_test)))
+rf = RandomForestRegressor(n_estimators=100).fit(X_train_poly, y_train)
+print("Score without interactions:{:.3f}".format(rf.score(X_test_poly,y_test)))
+# %%
+rnd = np.random.RandomState(0)
+X_org=rnd.normal(size=(1000,3))
+w = rnd.normal(size=3)
+
+X=rnd.poisson(10*np.exp(X_org))
+y=np.dot(X_org,w)
+
+# %%
+print("Number if feature appearances:\n{}".format(np.bincount(X[:,0])))
+
+# %%
+bins= np.bincount(X[:,0])
+plt.bar(range(len(bins)),bins,color='b')
+plt.ylabel("Number of appearances")
+plt.xlabel("Value")
+# %%
+from sklearn.linear_model import Ridge
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=0)
+score = Ridge().fit(X_train,y_train).score(X_test,y_test)
+print("Test score:{:.3f}".format(score))
+
+# %%
+X_train_log = np.log(X_train+1)
+X_test_log = np.log(X_test+1)
+# %%
+plt.hist(X_train_log[:,0],bins=25, color='gray')
+plt.ylabel("Number of appearances")
+plt.xlabel("Value")
+# %%
+score = Ridge().fit(X_train_log,y_train).score(X_test_log,y_test)
+print("Test score:{:.3f}".format(score))
+
+# %%
+from sklearn.datasets import load_breast_cancer
+from sklearn.feature_selection import SelectPercentile
+from sklearn.model_selection import train_test_split
+
+cancer = load_breast_cancer()
+# get deterministic random numbers
+rng = np.random.RandomState(42)
+noise  = rng.normal(size=(len(cancer.data),50))
+# add noise features to the data
+# the first 30 features are from the dataset, the next 50 are noise
+X_w_noise = np.hstack([cancer.data,noise])
+
+X_train, X_test,y_train,y_test = train_test_split(X_w_noise, cancer.target, random_state=0,test_size=.5)
+#use f_classif(the default) and SelectPercentile to select 50% of features
+select = SelectPercentile(percentile=50)
+select.fit(X_train,y_train)
+#transform training set
+X_train_selected = select.transform(X_train)
+
+print("X_train.shape:{}".format(X_train.shape))
+print("X_train_selected.shape:{}.".format(X_train_selected.shape))
+# %%
+mask=select.get_support()
+print(mask)
+#visualize the mask - - black is True, white is False
+plt.matshow(mask.reshape(1,-1),cmap='gray_r')
+plt.xlabel("Sample index")
+plt.yticks(()
+)
+
+
+# %%
+from sklearn.linear_model import LogisticRegression
+
+#transform test data
+
+X_test_selected = select.transform(X_test)
+
+lr = LogisticRegression()
+lr.fit(X_train,y_train)
+print("Score with all features: {:.3f}".format(lr.score(X_test, y_test)))
+lr.fit(X_train_selected, y_train)
+print("Score with only selected features: {:.3f}".format(
+lr.score(X_test_selected, y_test)))
+
+# %%
+from sklearn.feature_selection import SelectFromModel
+from sklearn.ensemble import RandomForestClassifier
+
+
+select = SelectFromModel(RandomForestClassifier(n_estimators=100, random_state=42), threshold="median")
+# %%
+select.fit(X_train,y_train)
+X_train_l1 = select.transform(X_train)
+print("X_train.shape:{}".format(X_train.shape))
+print("X_train_l1.shape:{}".format(X_train_l1.shape))
+# %%
+mask = select.get_support()
+#visualize the mask  - - black is True, white is False
+plt.matshow(mask.reshape(1,-1),cmap='gray_r')
+plt.xlabel("Sample index")
+plt.yticks(())
+# %%
+X_test_l1 = select.transform(X_test)
+score = LogisticRegression().fit(X_train_l1, y_train).score(X_test_l1,y_test)
+print("Test score:{:.3f}".format(score))
+# %%
+from sklearn.feature_selection import RFE
+select = RFE(RandomForestClassifier(n_estimators=100,random_state=42),n_features_to_select=40)
+select.fit(X_train,y_train)
+#visualise the selected features:
+mask = select.get_support()
+
+plt.matshow(mask.reshape(1,-1),cmap='gray_r')
+plt.xlabel("Sample index")
+plt.yticks(())
+# %%
+print("Test scoreL {:.3f}".format(select.score(X_test,y_test)))
+# %%
+citibike = mglearn.datasets.load_citibike()
+# %%
+print("Citi Bike data:\n{}".format(citibike.head()))
+# %%
+plt.figure(figsize=(10,3))
+xticks = pd.date_range(start=citibike.index.min(), end=citibike.index.max(),freq="D")
+plt.xticks(xticks,xticks.strftime("%a %m-%d"),rotation=90,ha="left")
+plt.plot(citibike,linewidth=1)
+plt.xlabel("Date")
+plt.ylabel("Rentals")
+
+
+# %%
+#extract the target values (number oif rentals)
+
+y = citibike.values
+
+#convert to POSIX time by dividing by 10**9
+X = citibike.index.astype("int64").values.reshape(-1,1)//10**9
+
+
+
+
+# %%
+# use the first 184 data points for training, and the rest for testing
+n_train = 184
+#function to evaluate and plot a regressor on a given feature set
+def eval_on_features(features, target, regressor):
+    #split the given featurs into a training and a test set
+    X_train,X_test = features[:n_train],features[n_train:]
+    # also split the target array
+    y_train,y_test = target[:n_train],target[n_train:]
+    regressor.fit(X_train,y_train)
+    print("Test0set R^2:{:.2f}".format(regressor.score(X_test,y_test)))
+    y_pred = regressor.predict(X_test)
+    y_pred_train = regressor.predict(X_train)
+    plt.figure(figsize=(10,3))
+    plt.xticks(range(0,len(X),8),xticks.strftime("%a %m -%d"), rotation=90,ha="left")
+
+    plt.plot(range(n_train),y_train,label="train")
+    plt.plot(range(n_train, len(y_test) + n_train), y_test, '-', label="test")
+    plt.plot(range(n_train), y_pred_train, '--', label="prediction train")
+    plt.plot(range(n_train, len(y_test) + n_train), y_pred, '--', label="prediction test")
+    plt.legend(loc=(1.01, 0))
+    plt.xlabel("Date")
+    plt.ylabel("Rentals")
+
+# %%
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=100,random_state=0)
+eval_on_features(X,y,regressor)
+# %%
+X_hour = citibike.index.hour.values.reshape(-1,1)
+eval_on_features(X_hour,y,regressor)
+# %%
+X_hour_week = np.hstack([citibike.index.dayofweek.values.reshape(-1,1),citibike.index.hour.values.reshape(-1,1)])
+eval_on_features(X_hour_week,y,regressor)
+# %%
+from sklearn.linear_model import LinearRegression
+eval_on_features(X_hour_week,y,LinearRegression())
+# %%
+enc = OneHotEncoder()
+X_hour_week_onehot=enc.fit_transform(X_hour_week).toarray()
+# %%
+eval_on_features(X_hour_week_onehot, y, Ridge())
+
+# %%
+poly_transformer = PolynomialFeatures(degree=2,interaction_only=True, include_bias=False)
+X_hour_week_onehot_poly=poly_transformer.fit_transform(X_hour_week_onehot)
+lr = Ridge()
+eval_on_features(X_hour_week_onehot_poly,y,lr)
+# %%
+hour = ["%02d:00" % i for i in range(0, 24, 3)]
+day = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+features = day + hour
+
+# %%
+features_poly = poly_transformer.get_feature_names(features) 
+features_nonzero = np.array(features_poly)[lr.coef_ != 0] 
+coef_nonzero = lr.coef_[lr.coef_ != 0]
+# %%
+plt.figure(figsize=(15, 2))
+plt.plot(coef_nonzero, 'o')
+plt.xticks(np.arange(len(coef_nonzero)), features_nonzero, rotation=90) plt.xlabel("Feature name")
+plt.ylabel("Feature magnitude")
